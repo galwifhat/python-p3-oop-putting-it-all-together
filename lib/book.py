6a4622b24df434ac4173845e8fd35de5cedf78e1
Ruth <cirurue@gmail.com>
2025-05-24 14:50:28 +0300

    oop done

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,22 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
+    def __init__(self, title, page_count = 7):
+        self.title = title
+        self.page_count = page_count
+    @property
+    def page_count(self):
+        return self._page_count
     
+    @page_count.setter
+    def page_count(self, page_count):
+        if not type(page_count) in (int,float):
+            print("page_count must be an integer")
+        self._page_count = page_count
+
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
+
+book = Book("The World According to Garp", 69)
+print(book.turn_page())
         
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,23 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__(self, brand, size):
+        self.brand = brand
+        self.size = size
+
+    @property
+    def size(self):
+        return self._size
+    @size.setter
+    def size(self, size):
+        if isinstance(size, int): # if I use'type' it is not iterable
+            self._size = size
+        else:
+            print("size must be an integer")
+            #raise ValueError("size must be an integer")
+    def cobble(self):
+        print("Your shoe is as good as new!")
+        self.condition = "New"
+
+kshoe = Shoe("Addidas", 9)
+kshoe.cobble()
